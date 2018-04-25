ea0783bf30004dc7e63ed30081c7401beda7369f
barreira266 <barreira266@hotmail.com>
2018-04-25 14:19:16 -0400

    lazy load

---
+++ src/pages/home/home.module.ts
@@ -0,0 +1,16 @@
+//lazy loda
+
+import { IonicPageModule } from 'ionic-angular';
+import { NgModule } from '@angular/core';
+
+import { HomePage } from './home';
+
+
+
+@NgModule({
+  declarations: [HomePage],
+  imports: [IonicPageModule.forChild(HomePage)]
+})
+export class HomeModule {
+  
+}
